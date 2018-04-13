55a608ce1d4116bb30cd59d4cad0f463d9987970
Aman Dwivedi <dwivedi.aman96@gmail.com>
2018-04-13 19:21:03 +0530

    some tests added

--- tests/test.js
+++ tests/test.js
@@ -17,6 +17,51 @@ export const test = (server) => {
 					done();
 				});
 		});
+
+		it('GET /vote', function (done) {
+			chai.request(server)
+				.get('/vote')
+				.end(function (err, res) {
+					res.should.have.status(200);
+					done();
+				});
+		});
+
+		it('GET /register', function (done) {
+			chai.request(server)
+				.get('/register')
+				.end(function (err, res) {
+					res.should.have.status(200);
+					done();
+				});
+		});
+
+		it('GET /admin', function (done) {
+			chai.request(server)
+				.get('/admin')
+				.end(function (err, res) {
+					res.should.have.status(200);
+					done();
+				});
+		});
+
+		it('GET /admin/login', function (done) {
+			chai.request(server)
+				.get('/admin/login')
+				.end(function (err, res) {
+					res.should.have.status(200);
+					done();
+				});
+		});
+
+		it('GET /results', function (done) {
+			chai.request(server)
+				.get('/results')
+				.end(function (err, res) {
+					res.should.have.status(200);
+					done();
+				});
+		});
 	});
 
 }
