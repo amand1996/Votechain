6e5050c25fd3e43fbf71164002282cc230e5376d
Aman Dwivedi <dwivedi.aman96@gmail.com>
2018-05-13 12:14:42 +0530

    updated api

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-    microsoftApiKey: 'e5716d43c5df4057a369b8cbaa4cf319',
+    microsoftApiKey: '06b5c355846b4eff8074a08230765499',
     microsoftDetectURL: "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect",
     microsoftVerifyURL: 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/verify',
     mongoURI: 'mongodb://votechain:votechain@ds127589.mlab.com:27589/votechain'
